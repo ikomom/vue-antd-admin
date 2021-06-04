45414a63dee8cc6918ebb7494a7b6108b22b4774
ikonon <315793076@qq.com>
2021-06-04 23:28:21 +0800

    revert

--- src/components/task/TaskGroup.vue
+++ src/components/task/TaskGroup.vue
@@ -23,9 +23,9 @@ const dragOptions = {
   scroll: true,
   scrollSpeed: 2,
   animation: 150,
-  ghostClass: 'employTest-ghost',
-  chosenClass: 'employTest-chose',
-  dragClass: 'employTest-drag'
+  ghostClass: 'dragable-ghost',
+  chosenClass: 'dragable-chose',
+  dragClass: 'dragable-drag'
 }
 
 export default {
